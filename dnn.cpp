09a7a42390311caa6f2c1b0392ff075ccc489263
Jiajie Chen <jiegec@qq.com>
2018-10-24 14:58:35 +0800

    Add tiny-dnn

---
+++ dnn.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <tiny_dnn/tiny_dnn.h>
+
+using namespace tiny_dnn;
+using namespace tiny_dnn::layers;
+using namespace tiny_dnn::activation;
+
+int main() {
+    network<sequential> net;
+    return 0;
+}
